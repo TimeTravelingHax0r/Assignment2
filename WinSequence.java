1fbd20b876e93c8e9ef341364fd13d1862096b89
songborn <116478084+songborn@users.noreply.github.com>
2022-11-10 16:40:08 -0800

    Update WinSequence.java
    provided method for winSequence

--- WinSequence.java
+++ WinSequence.java
@@ -1,5 +1,33 @@
-public class WinSequence {
-    public WinSequence() {
-        
+public class winSequence{
+	private int highScore = 0;
+	pirvate int winners = 0;
+	private int[] scores = new int[gameManagers.size()];
+	public winSequence(){
+		for(player i : players i){
+			scores[i] = players(i).getDollars + players(i).getCredits + (5 * players(i).getRank);
+			if(iscores[i]  == highScores){
+				winners++;
+			}elif (scores[i] > highscore){
+				Winners = 1;
+				highScore = scores[i]
+            }
+		}	
+	}
+	if(winners > 1){
+	    System.out.print(“The winners are  “);
+    for(player i : players i){
+        if(i.getScore == highScore){
+	        System.out.println(i.getName + “ “);
+        }
     }
+    System.out.println(“ with “ + highScore + “ points.”);
+}else{
+	System.out.print(“The winner is  “);
+    for(player i : players i){
+	    if(i.getScore == highScore){
+		    System.out.println(i.getName);
+        }
+    }
+    System.out.println(“ with “ +highScore + “ points.”);
 }
+
